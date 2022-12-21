1c144a651c19096cc8987d499d87989087a18861
HaradaYasuo <academic.dev0531@gmail.com>
2022-12-22 01:23:42 +0530

    changed backend URL

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ import 'aos/dist/aos.css';
 
 function App() {
 
-  let socket=io("https://siren-blog.up.railway.app/")
+  let socket=io("https://siren-blog-backend.onrender.com")
   let stateElement=useSelector((state)=>{return state})
   let dispatch=useDispatch()
 
